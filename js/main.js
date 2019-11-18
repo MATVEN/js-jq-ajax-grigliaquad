5d9de40caedd857fa602e4b4ff4a540f1f0822e5
MATVEN <mattiavena2993@gmail.com>
2019-11-18 17:13:33 +0100

    add condition click

--- js/main.js
+++ js/main.js
@@ -7,5 +7,33 @@ function generateGrill() {
 
 // Api
 $(document).ready(function() {
+
   generateGrill();
+
+  // selection cell
+  var items = $('.items');
+
+  items.click(showCell);
+
+  function showCell() {
+     var itemsClick = $(this);
+
+     // ajax call
+     $.ajax({
+       url: "https://flynn.boolean.careers/exercises/api/random/int",
+       method: 'GET',
+       succes: function () {
+          // Se il numero è <= 5 diventa giallo
+         if (data.response <= 5 && !itemsClick.hasClass('active')) {
+
+           itemsClick.css('background-color', 'yellow').addClass('active').html(data.response);
+           // se è > di 5 il quadrato diventa verde.
+         }else if (data.response > 5 && !itemsClick.hasClass('active')) {
+
+           itemsClick.css('background-color', 'green').addClass('active').html(data.response);
+
+         }
+       }
+     })
+  }
 })
